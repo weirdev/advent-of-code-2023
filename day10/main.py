86e798264ef4c00df5ab4fa87b5d9e1f44c78aa4
Wesley Weirather <wweirather@fb.com>
2023-12-17 08:22:54 -0600

    Day 10, Part 2: Completed

--- day10/main.py
+++ day10/main.py
@@ -1,4 +1,5 @@
-from typing import List, Iterator, Tuple
+from typing import List, Iterator, Tuple, Optional, Dict
+
 
 class Pipe:
     def __init__(self, char: str, start: (int, int), end: (int, int)):
@@ -11,6 +12,7 @@ class Pipe:
         self.start = start
         self.end = end
 
+
 # y=0 is the top row
 WE = Pipe('-', (-1, 0), (1, 0))
 NS = Pipe('|', (0, -1), (0, 1))
@@ -22,6 +24,7 @@ EN = Pipe('L', (1, 0), (0, -1))
 PIPES = [WE, NS, SE, WS, NW, EN]
 PIPES_BY_CHAR = {pipe.char: pipe for pipe in PIPES}
 
+
 def adj_points_iter(grid: List[List[str]], x: int, y: int) -> Iterator[Tuple[int, int]]:
     if x > 0:
         yield (x-1, y)
@@ -32,15 +35,18 @@ def adj_points_iter(grid: List[List[str]], x: int, y: int) -> Iterator[Tuple[int
     if y < len(grid) - 1:
         yield (x, y+1)
 
-def connected_omni_dir(grid: List[List[str]], x: int, y: int) -> [(int, int)]:
+
+def connected_omni_dir(grid: List[List[str]], x: int, y: int, allow_omni_to_omni: bool = False) -> [(int, int)]:
     connections = []
     for i, j in adj_points_iter(grid, x, y):
         if i == x and j == y:
             continue
         char = grid[j][i]
-        if char == 'S': # (x,y) is probably the only S, but explicitly handle connecting to one just in case
-            # connections.append((i, j))
-            raise Exception("Omni direction connection (S) to (another) S")
+        if char == 'S':
+            if not allow_omni_to_omni:
+                # (x,y) is probably the only S, but explicitly handle connecting to one just in case
+                raise Exception("Omni direction connection (S) to (another) S")
+            connections.append((i, j))
         elif char != '.': # (x,y) is probably the only S, but exclude it just in case
             pipe = PIPES_BY_CHAR[char]
             for con in pipe.start, pipe.end:
@@ -49,6 +55,7 @@ def connected_omni_dir(grid: List[List[str]], x: int, y: int) -> [(int, int)]:
                     connections.append((i, j))
 
     return connections
+
 
 def get_start_first_and_last_points(grid: List[List[str]]) -> ((int, int), (int, int), (int, int)):
     for i in range(len(grid[0])):
@@ -61,6 +68,7 @@ def get_start_first_and_last_points(grid: List[List[str]]) -> ((int, int), (int,
                 return ((i, j), start, end)
 
     raise Exception("No start point (S) found")
+
 
 def get_next_point(grid: List[List[str]], curr: (int, int), prev: (int, int)) -> (int, int):
     x, y = curr
@@ -75,14 +83,16 @@ def get_next_point(grid: List[List[str]], curr: (int, int), prev: (int, int)) ->
             return (pos_next_x, pos_next_y)
     raise Exception(f"Couldn't find next point for {curr}")
 
-def traverse(grid: List[List[str]], start: (int, int), start_prev: (int, int), end: (int, int)) -> int:
+
+def traverse(grid: List[List[str]], start: (int, int), start_prev: (int, int), end: (int, int)) -> List[Tuple[int, int]]:
     path = [start_prev, start]
     while path[-1] != end:
         path.append(get_next_point(grid, path[-1], path[-2]))
 
-    return len(path)
+    return path
 
-def traverse_from_start(grid: List[List[str]]) -> int:
+
+def traverse_from_start(grid: List[List[str]]) -> List[Tuple[int, int]]:
     start_prev, start, end = get_start_first_and_last_points(grid)
     return traverse(grid, start, start_prev, end)
 
@@ -91,11 +101,164 @@ def part1(filename: str) -> int:
     with open(filename, "r") as f:
         lines = list(map(lambda line: list(line.strip()), f.readlines()))
 
-    path_len = traverse_from_start(lines)
+    path_len = len(traverse_from_start(lines))
 
     return path_len // 2
+
+
+def adj_half_to_full_pos_iter(grid: List[List[str]], full: (int, int)) -> Iterator[Tuple[int, int]]:
+    x = full[0] * 2 + 1
+    y = full[1] * 2 + 1
+
+    if x > 0:
+        yield (x-1, y)
+    if x < len(grid[0]) * 2:
+        yield (x+1, y)
+
+    if y > 0:
+        yield (x, y-1)
+    if y < len(grid) * 2:
+        yield (x, y+1)
+
+
+def adj_half_to_half_iter(grid: List[List[str]], half: (int, int)):
+    x, y = half
+    if x % 2 == 0:
+        # Vertical
+        assert y % 2 == 1, "Half identifies line, not point"
+
+        if y > 0:
+            if y > 2:
+                # Up vertical
+                yield (x, y-2)
+            if x > 0:
+                # Up half left 90 deg
+                yield (x-1, y-1)
+            if x < len(grid[0]) * 2:
+                # Up half right 90 deg
+                yield (x+1, y-1)
+        if y < len(grid) * 2:
+            if y < (len(grid) * 2) - 2:
+                # Down vertical
+                yield (x, y+2)
+            if x > 0:
+                # Down half left 90 deg
+                yield (x-1, y+1)
+            if x < len(grid[0]) * 2:
+                # Down half right 90 deg
+                yield (x+1, y+1)
+    elif y % 2 == 0:
+        # Horizontal
+        assert x % 2 == 1, "Half identifies line, not point"
+
+        if x > 0:
+            if x > 2:
+                # Left horizontal
+                yield (x-2, y)
+            if y > 0:
+                # Left half up 90 deg
+                yield (x-1, y-1)
+            if y < len(grid) * 2:
+                # Left half down 90
+                yield (x-1, y+1)
+        if x < len(grid[0]) * 2:
+            if x < (len(grid[0]) * 2) - 2:
+                # Right horizontal
+                yield (x+2, y)
+            if y > 0:
+                # Right half up 90 deg
+                yield (x+1, y-1)
+            if y < len(grid) * 2:
+                # Right half down 90
+                yield (x+1, y+1)
+    else:
+        raise Exception("Half identifies grid cell, not line")
+
+
+def adj_breaks_conn(grid: List[List[str]], half: (int, int)) -> Optional[Tuple[Tuple[int, int], Tuple[int, int]]]:
+    x = half[0] // 2
+    y = half[1] // 2
+    if half[0] % 2 == 0:
+        # Vertical
+        assert half[1] % 2 == 1, "Half identifies line, not point"
+
+        if x > 0 and x < len(grid[0]):
+            return ((x-1, y), (x, y))
+        return None
+    elif half[1] % 2 == 0:
+        # Horizontal
+        assert half[0] % 2 == 1, "Half identifies line, not point"
+
+        if y > 0 and y < len(grid):
+            return ((x, y-1), (x, y))
+        return None
+    else:
+        raise Exception("Half identifies grid cell, not line")
+
+
+def path_to_dict(path: List[Tuple[int, int]]) -> Dict[Tuple[int, int], Tuple[int, int]]:
+    d = {}
+    for i in range(len(path)-1):
+        d[path[i]] = path[i+1]
+    d[path[-1]] = path[0]
+    return d
+
+
+def color_outside_path(grid: List[List[str]], path: List[Tuple[int, int]]) -> int:
+    path_dict = path_to_dict(path)
+
+    ## Find all the outside half positions
+    outside_by_halfs = [] # Results
+    outside_halfs_to_visit = [(0,1)] # Queue
+    checked_halfs = set(outside_by_halfs) # Prevent checking the same half multiple times
+    while len(outside_halfs_to_visit) > 0:
+        outside_half = outside_halfs_to_visit.pop()
+
+        for adj in adj_half_to_half_iter(grid, outside_half):
+            if adj in checked_halfs:
+                continue
+            checked_halfs.add(adj)
+
+            broken_conn_opt = adj_breaks_conn(grid, adj)
+            if broken_conn_opt is not None:
+                start, end = broken_conn_opt
+                if (start in path_dict and path_dict[start] == end) or (end in path_dict and path_dict[end] == start):
+                    continue # Cannot color through pipe path
+
+            outside_by_halfs.append(adj)
+            outside_halfs_to_visit.append(adj)
+
+    ## Color each full position that
+    ## 1. Is not a pipe
+    ## 2. Is adjacent to an outside half
+    outside_by_halfs = set(outside_by_halfs)
+    # marked_grid = [list(row) for row in grid]
+    marks = 0
+    for x in range(len(grid[0])):
+        for y in range(len(grid)):
+            if (x, y) in path_dict:
+                continue
+            for adj in adj_half_to_full_pos_iter(grid, (x, y)):
+                if adj in outside_by_halfs:
+                    # marked_grid[y][x] = "#"
+                    marks += 1
+                    break
+
+    return marks
+
+
+def part2(filename: str) -> int:
+    with open(filename, "r") as f:
+        grid = list(map(lambda line: list(line.strip()), f.readlines()))
+
+    path = traverse_from_start(grid)
+    marks = color_outside_path(grid, path)
+
+    return (len(grid) * len(grid[0])) - marks - len(path)
 
 
 if __name__ == "__main__":
     p1 = part1("input")
     print(f"Part 1: {p1}")
+    p2 = part2("input")
+    print(f"Part 2: {p2}")
